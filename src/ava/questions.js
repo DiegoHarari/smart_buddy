0601f895c0529c9b2bf958ce98294866fbae85cf
Diego Harari <diehara@gmail.com>
2020-08-25 14:30:43 -0300

    ava wip

---
+++ src/ava/questions.js
@@ -0,0 +1,1 @@
+const keywords = ["ticket", "desk"]; // dos kywords
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,22 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 const data = require("../data/data.json");
+const keywords = require("../ava/questions");
 const cors = require("cors");
 
 app.use(cors());
 router.get("/", async (req, res) => {
   try {
     res.status(200).send(data);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+router.get("/ava/:question", async (req, res) => {
+  const question = req.params.question.split(" "); //aca recibe la pregunta spliteada
+  try {
+    res.status(200).send(JSON.parse(question));
   } catch (e) {
     res.status(500).send(e);
   }
